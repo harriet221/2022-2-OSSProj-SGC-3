c221d2e43107939ebfc014a50b568d572aea3cbf
irina0627 <seyon0627@naver.com>
2022-12-09 04:27:02 +0900

    FIX mainMenu.py 창 크기 width, height and->or 조건으로 수정

--- mainMenu.py
+++ mainMenu.py
@@ -384,7 +384,7 @@ else:
                 break
             if event.type == pygame.VIDEORESIZE:
                 # Update the surface
-                if event.w <= Display.minscreen_x.value and event.h <= Display.minscreen_y.value:
+                if event.w <= Display.minscreen_x.value or event.h <= Display.minscreen_y.value:
                     screen = pygame.display.set_mode((Display.minscreen_x.value, Display.minscreen_y.value),
                                                  pygame.RESIZABLE)
                 else:
