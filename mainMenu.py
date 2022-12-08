f3739961a4569ac14dffeda8af35965a1d23c767
gaeun <geun5744@gmail.com>
2022-12-08 17:31:51 +0900

    ADD mainMenu.py 주석 추가

--- mainMenu.py
+++ mainMenu.py
@@ -10,7 +10,7 @@ pygame.mixer.init()
 
 
 # game variables
-#bgmsound = pygame.mixer.Sound(Sounds.bgm.value)
+bgmsound = pygame.mixer.Sound(Sounds.bgm.value)
 
 pygame.init()
 infoObject = pygame.display.Info()
@@ -37,13 +37,6 @@ def show_signinup():
     menu.add.button(Content.quit_btn.value, pygame_menu.events.EXIT)
 
 # 로그인 후 보여지는 메뉴 화면
-
-
-def start_the_game():
-    from mainGame import startGame
-    startGame(True)
-
-
 def show_mode():
     menu.clear()
     menu.add.image(Images.logo.value,
@@ -56,25 +49,26 @@ def show_mode():
     menu.add.toggle_switch(Content.sound_btn.value, False, sound)
     menu.add.button(Content.quit_btn.value, pygame_menu.events.EXIT)
 
+def start_the_game():
+    from mainGame import startGame
+    startGame(True)
 
-def rank():
+def rank(): # 랭킹페이지
     menu.clear()
-    table=menu.add.table(table_id=Content.tb_rank.value)
+    table=menu.add.table(table_id=Content.tb_rank.value) # 랭킹 테이블 생성
     table.default_cell_padding=Display.rank_padding.value
     table.default_cell_align=pygame_menu.locals.ALIGN_CENTER
     table.add_row(Content.rank_rowname.value,cell_font=pygame_menu.font.FONT_OPEN_SANS_BOLD,cell_padding=Display.rank_idx_padding.value)
 
-    rank_list=dataLoad.rankList_get() # 랭킹리스트 가져오기
+    rank_list=dataLoad.rankList_get() # DB에서 랭킹리스트 가져오기
     for rank in rank_list:
-        rank[2]=format(rank[2],',')
+        rank[2]=format(rank[2],',') # 큰 숫자는 ,로 구분
         table.add_row(rank,cell_font_size=Display.description_fontsize.value)
 
     menu.add.vertical_margin(Display.small_margin.value)
-    menu.add.button(Content.back_btn.value, show_mode)
+    menu.add.button(Content.back_btn.value, show_mode) # 뒤로가기 버튼
 
 # help 페이지
-
-
 def help():
     menu.clear()
     menu.add.label(Content.help_title.value, font_size=Display.title_fontsize.value, padding=Display.padding_large.value)
@@ -118,8 +112,6 @@ def help():
     menu.add.button(Content.back_btn.value, show_mode)
 
 # about 페이지
-
-
 def about():
     menu.clear()
     menu.add.label(Content.about_title.value, font_size=Display.title_fontsize.value,
@@ -142,46 +134,46 @@ def about():
     menu.add.button(Content.back_btn.value, show_mode)
 
 # True가 반환될경우 소리가 켜지고 아니면 꺼짐
-
-
 def sound(sound):
     if sound == True:
         bgmsound.play()
     else:
         bgmsound.stop()
 
-# 회원가입 기능
 
+# 회원가입 기능
 def sign_up():
     menu.clear()
-    email = menu.add.text_input(Content.email_input.value, id=Content.email.value)
-    password = menu.add.text_input(Content.pw_input.value, password=True, id=Content.pw.value)
+    email = menu.add.text_input(Content.email_input.value, id=Content.email.value) # 이메일 입력
+    password = menu.add.text_input(Content.pw_input.value, password=True, id=Content.pw.value) # 비밀번호 입력
     menu.add.label(Content.pwref.value, font_size=Display.reference_fontsize.value)
-    conFirmPassword = menu.add.text_input(Content.confirm_pw_input.value, password=True, id=Content.pw.value)
-    menu.add.button(Content.submit_btn.value, sign_up_button, email, password, conFirmPassword)
+    conFirmPassword = menu.add.text_input(Content.confirm_pw_input.value, password=True, id=Content.pw.value) # 비밀번호 재입력
+    menu.add.button(Content.submit_btn.value, sign_up_button, email, password, conFirmPassword) # 제출버튼
     menu.add.button(Content.back_btn.value, show_signinup)
+
+
 # 회원가입 제출 버튼
 def sign_up_button(email, password, conFirmPassword):
     registerReturn = register.register(
         email.get_value(), password.get_value(), conFirmPassword.get_value())
-    if registerReturn == 1:
+    if registerReturn == 1: # 회원가입 성공시, 성공 메시지 출력
         print(pg.alert(text=Content.signupmsg.value, title=Content.signup.value))
     else:
         print(pg.alert(text=Content.errormsg.value, title=Content.error.value))
 
-# 비밀번호 재설정 버튼
 
+# 비밀번호 재설정 페이지
 def resetPassword():
     menu.clear()
     email = menu.add.text_input(Content.email_input.value, id=Content.email.value)
     menu.add.button(Content.submit_btn.value, resetPassword_Button, email)
-    menu.add.button(Content.signin_btn.value, login)
+    menu.add.button(Content.signin_btn.value, login) # 로그인 페이지로 이동
     menu.add.button(Content.back_btn.value, show_signinup)
 
 
-def resetPassword_Button(email):
-    register.passwordReset(email.get_value())
-    print(pg.alert(text=Content.resetmsg.value, title=Content.reset.value))
+def resetPassword_Button(email): 
+    register.passwordReset(email.get_value()) # 비밀번호 재설정 수행
+    print(pg.alert(text=Content.resetmsg.value, title=Content.reset.value)) 
 
 # 로그인
 
@@ -203,32 +195,32 @@ def loginButton(email, password):
     if login != 0:  # 로그인에 성공하면 다음으로 넘어감
         show_mode()  # 메인 메뉴 페이지로 넘어가기
     else:
-        print(pg.alert(text=Content.errormsg.value, title=Content.error.value))
+        print(pg.alert(text=Content.errormsg.value, title=Content.error.value)) # 실패시, 실패 안내 메시지창 출력
 
 # 상점
 def store():
     menu.clear()
     menu.add.label(Content.store_title.value, font_size=Display.title_fontsize.value, padding=Display.padding_large.value)  # page title
-    menu.add.button(Content.buyitem_btn.value,Buy_page)
-    menu.add.button(Content.applyitem_btn.value,apply_item_page)
-    menu.add.button(Content.givecoin_btn.value,give_coin_page)
+    menu.add.button(Content.buyitem_btn.value,Buy_page) # 아이템 구매페이지로 이동
+    menu.add.button(Content.applyitem_btn.value,apply_item_page) # 아이템 적용 페이지로 이동
+    menu.add.button(Content.givecoin_btn.value,give_coin_page) # 코인 선물 페이지
     menu.add.vertical_margin(Display.small_margin.value)
-    menu.add.button(Content.back_btn.value, show_mode)
+    menu.add.button(Content.back_btn.value, show_mode) # 뒤로가기 버튼
 
 def Buy_page():
     menu.clear()
-    menu.add.label(Content.coin.value) # 현재 코인 표시
-    menu.add.label(dataLoad.coin_get(register.user))
+    menu.add.label(Content.coin.value)
+    menu.add.label(dataLoad.coin_get(register.user)) #  현재 코인 표시
     menu.add.label(Content.item_category.value)
     item_list= Content.items.value
-    buy_list = dataLoad.item_buyList_get(register.user)
-    for item in item_list:
-        if item in buy_list:
+    buy_list = dataLoad.item_buyList_get(register.user) # 사용자가 구매한 아이템 리스트 가져옴
+    for item in item_list: 
+        if item in buy_list: # 구매한 아이템일 경우, 흑색 이미지 사용 및 구매 버튼 비활성화
             image_path=Content.img_path.value + item + Content.img_have.value
             menu.add.image(image_path,
                         angle=Display.angle.value, scale=Display.medium_scale.value)
             menu.add.button(Content.buy_btn.value, Buy_check)
-        else:
+        else: # 구매하지 않은 아이템일 경우, 컬러 이미지 사용 및 구매 버튼 활성화
             weapon_image_path=Content.img_path.value+item+Content.img_size256.value
             price_image_path=Content.img_path.value+item+Content.img_price.value
             menu.add.image(weapon_image_path,
@@ -238,7 +230,7 @@ def Buy_page():
             menu.add.button(Content.buy_btn.value, Buy, register.user,item)
 
     menu.add.vertical_margin(Display.small_margin.value)
-    menu.add.button(Content.back_btn.value, store)
+    menu.add.button(Content.back_btn.value, store) # 뒤로가기 버튼
 
 
 def apply_item_page():
@@ -263,27 +255,27 @@ def apply_item_page():
 def give_coin_page(): # 코인 선물 페이지
     menu.clear()
     menu.add.label(Content.gift_info.value)
-    friend_email = menu.add.text_input(Content.email_input.value)
-    coin = menu.add.text_input(Content.coin_input.value)
-    menu.add.button(Content.submit_btn.value,giveButton,friend_email,coin)
+    friend_email = menu.add.text_input(Content.email_input.value) # 친구 이메일 입력
+    coin = menu.add.text_input(Content.coin_input.value) # 선물하고자 하는 코인량 입력
+    menu.add.button(Content.submit_btn.value,giveButton,friend_email,coin) # 입력값 제출 버튼
 
     menu.add.vertical_margin(Display.small_margin.value)
-    menu.add.button(Content.back_btn.value, store)
+    menu.add.button(Content.back_btn.value, store) # 뒤로가기 버튼
 
 def giveButton(friend_email,coin): 
-    dataLoad.coin_give(register.email,friend_email.get_value(),coin.get_value())
+    dataLoad.coin_give(register.email,friend_email.get_value(),coin.get_value()) # 친구에게 메일 선물하기 기능 수행
     print(pg.alert(text=Content.giveok_msg.value, title=Content.giveok_msgtitle.value))
 
 def Buy(user,item): #아이템 구매 함수
     dataLoad.item_buy(user,item)
 
-def Buy_check():
+def Buy_check(): # 이미 구매한 아이템의 경우, 안내 메시지 출력
     print(pg.alert(text=Content.have_msg.value, title=Content.have_msgtitle.value))
 
-def apply_current_item(user,item):
+def apply_current_item(user,item): # 게임play에 아이템 적용 함수
     dataLoad.item_apply(user,item)
 
-def give_coin(user,coin):
+def give_coin(user,coin): # 코인 선물하기 함수
     dataLoad.coin_give(user,coin)
 
 # 여기서부터가 메인화면
